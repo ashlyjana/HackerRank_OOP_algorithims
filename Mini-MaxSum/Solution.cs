c6d5bc43499c3142086db7fba0bef76cf0accdb8
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-09-25 14:28:58 +0800

    Create Solution.cs

---
+++ Mini-MaxSum/Solution.cs
@@ -0,0 +1,54 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    { long sum = 0;
+        int min = arr[0];
+        int max = arr[0];
+
+        foreach (int num in arr)
+        {
+            sum += num;
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        long minSum = sum - max;
+        long maxSum = sum - min;
+
+        Console.WriteLine(minSum + " " + maxSum);
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
